830decaff33eff8dcc1fe3c9ff6a6df96b913fd9
AleexCaires <tiagoaleex98@gmail.com>
2022-09-27 23:32:31 +0100

    First Exercices

---
+++ exercises/02.py
@@ -0,0 +1,13 @@
+# Intermediate Expressions
+
+# Ex 2
+name = input("Enter your name: ")
+print("Hello", name)
+
+# Ex 3
+
+hour = input("Enter your Work Hours: ")
+rate = input("Enter your rate: ")
+pay = float(hour) * float(rate)
+
+print(pay)
