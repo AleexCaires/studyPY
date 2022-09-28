63833dcd5105c3b50c399b7f6b9f26aa74759590
AleexCaires <tiagoaleex98@gmail.com>
2022-09-28 22:49:16 +0100

    Added new Exercise into the File

--- exercises/02.py
+++ exercises/02.py
@@ -11,3 +11,20 @@ rate = input("Enter your rate: ")
 pay = float(hour) * float(rate)
 
 print(pay)
+
+# Ex 6
+
+def computepay() :
+    Hours = input("Enter hours:")
+ 
+    H = float(Hours)
+    if H > 40 :
+        H = (H - 40) * 1.5 + 40
+    else :
+        H = H
+    Rate = input("Enter rate:")
+    R = float(Rate)
+    Payment = H * R 
+    print("Payment:", Payment) 
+    
+computepay()
