efc56f49a5c7f01cd2d6f9b4c23ba56df1fdbcf2
AleexCaires <tiagoaleex98@gmail.com>
2022-10-08 23:07:58 +0100

    Added new exercises

--- exercises/02.py
+++ exercises/02.py
@@ -28,3 +28,24 @@ def computepay() :
     print("Payment:", Payment) 
 
 computepay()
+
+#Find the largest Number
+
+largest_number = -1
+
+for the_num in [9,41,32,69,79,1,3,89] :
+    if the_num > largest_number :
+        largest_number = the_num
+    print(largest_number, the_num)
+
+
+
+#Code to find the smallest value from a list of values
+
+smallest = None
+print("Before:", smallest)
+for itervar in [3, 41, 12, 9, 74, 15]:
+    if smallest is None or itervar < smallest:
+        smallest = itervar
+    print("Loop:", itervar, smallest)
+print("Smallest:", smallest)
