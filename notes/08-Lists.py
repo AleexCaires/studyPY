980c55617606683a53aa323454747efc09e6f507
AleexCaires <tiagoaleex98@gmail.com>
2022-10-21 14:21:12 +0100

    Added Lists notes

---
+++ notes/08-Lists.py
@@ -0,0 +1,29 @@
+#Python 08 - Lists A
+
+# Lists constants --> [etc, etc]
+# Range Function returns a list of numbers
+
+fruit = 'banana'
+x = fruit[1]
+#value of x = a
+
+#Python 08 - Lists B
+
+# concatenating lists using (+)
+# lists can be sliced using (:)
+
+# Just like in string, the second number is up "up to but not including"
+
+#Build a list from scratch 
+stuff = list()
+stuff = append('book')
+
+# We can order a list with, sort()
+
+#Built-in functions: len; max; min; sum;
+
+#Python 08 - Lists C
+
+# String and Lists are best friends
+
+#Double Split pattern -> split a line one way and grab one of the pieces of the line and split that piece again
