2110176a57c45665dba52cd69080a68db5fe5786
AleexCaires <tiagoaleex98@gmail.com>
2022-10-24 16:10:35 +0100

    Added notes for Dictionaries A

---
+++ notes/09-Dictionaries.py
@@ -0,0 +1,8 @@
+# What is a collection (in memory data base) it's a situation where we have a variable, like a list or a dictionarie and we can put multiple pieces of information.
+
+# Dictionaries are like lists except that they use keys instead of numbers to look up values.
+
+# Dictionaries literals(constants) -> they use {}
+
+# 09 - Dictionaries B 
+
