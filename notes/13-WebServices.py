7eb8371c8f0ba042392da1493f5b47e4c1372508
AleexCaires <tiagoaleex98@gmail.com>
2023-03-03 20:51:55 +0000

    Added notes for Lessons 13

--- notes/13-WebServices.py
+++ notes/13-WebServices.py
@@ -3,3 +3,18 @@
 #XML(eXtensible Markup Language) - Marking up language to send across the network 
 
 
+#JSON represents data as nested "lists" and "dictionaries"
+
+#Service Oriented Approach - is a way we approach solving a complex application problem where the data that lives over one computer system is spread out over the internet connected via internet or internal network
+
+#Web Services
+
+#API- Application Programming Interface
+
+#Summary 
+
+# -  Service Oriented Architecture - allows an application to be broken into parts and distributed across a network
+# - An Application Program Interface(API)is a contract for interaction
+# - Web services provide infrastructure for applications cooperating(an API) over a network - SOAP and REST are two styles of web services
+# -  XML and JSON are serialization formats
+
