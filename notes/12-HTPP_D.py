742cf25d71768e3755589f2b1e7103317da6f22b
AleexCaires <tiagoaleex98@gmail.com>
2023-03-03 19:23:14 +0000

    Added new HTTP notes

---
+++ notes/12-HTPP_D.py
@@ -0,0 +1,17 @@
+#Characters and Strings
+
+#Representing Simple Strings
+print(ord('H'))
+print(ord('/n'))
+
+#Multi-byte Character 
+
+#UTF-8 is recommended practice for encoding data to be exchanged between systems
+
+#Two kinds of strings in Python
+
+#In python 3 all string internally are Unicode
+
+#Python strings to to Bytes
+
+#When we talk to an external resource like a network socket it sends bytes, so we need to encode Python 3 strings into a given character encoding 
