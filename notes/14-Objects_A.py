16bd6229aa6688bb66817ef30d291e765a6d860b
AleexCaires <tiagoaleex98@gmail.com>
2023-03-13 16:17:40 +0000

    Notes for Objects Classes

---
+++ notes/14-Objects_A.py
@@ -0,0 +1,5 @@
+#An Object is a bit of self contained Code and data
+# - A key aspect of the object approach is to break the problem into smaller understandable parts(divide and conquer)
+# - Objects have boundaries that allow us to ignore un-needed details
+
+
